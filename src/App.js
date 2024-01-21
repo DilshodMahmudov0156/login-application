5d985213ecb7627524707dfe1bc2d00fe5108273
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-21 07:54:48 +0500

    i created design for my 'perfect-login' application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Login from "./components/login-page/Login";
 
 function App() {
   return (
-    <div className="App text-center">
+    <div className="App">
+        <Login/>
     </div>
   );
 }
--- src/components/login-page/Login.js
+++ src/components/login-page/Login.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import './style.css';
 
 function Login() {
     return (
-        <div></div>
+        <div className="login-panel">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-xl-3 col-md-5 col-sm-7 col-10 login-parent">
+                        <h4 className="text-center">LogIn</h4>
+                        <form>
+                            <input type="email" placeholder="Enter_your_email_@..."/>
+                            <input type="password" placeholder="Enter_your_password****"/>
+                            <button className="enter-btn">Enter <i className="bi bi-hand-index-thumb"></i></button>
+                            <button className="enter-btn-2">LogIn with <i className="bi bi-google"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 

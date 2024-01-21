d01cd9a70890b22f057711dc0f4b71f6fecc37eb
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-21 07:03:03 +0500

    just empty project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App text-center">
-        <button className="btn btn-primary mt-5">
-            <h4>Let's go Dilshodjon</h4>
-        </button>
     </div>
   );
 }

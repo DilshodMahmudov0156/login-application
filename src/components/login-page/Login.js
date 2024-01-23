62a4d51cbf6afb907bd8eeb72030a57b7639a0a5
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-23 09:09:52 +0500

    i make this project like that can signIn-signUp wiht inputs

--- src/components/login-page/Login.js
+++ src/components/login-page/Login.js
@@ -1,12 +1,20 @@
 import './style.css';
-import {signInWithPopup, createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+import {signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 import {auth, provider} from "../../firebaseConfig";
 import {useState} from "react";
 
 function Login() {
 
+    const [inUp, setInUp] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleChangeFirst = () => {
+        setInUp(true);
+    }
+    const handleChangeSecond = () => {
+        setInUp(false);
+    }
 
     const getEmVal = (e) => {
         setEmail(e.target.value);
@@ -17,12 +25,22 @@ function Login() {
 
     const loginWithEmailPassword = (e) => {
         e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user)
-        }).catch((error) => {
-            console.log(error.message);
-        })
+        if (inUp){
+            signInWithEmailAndPassword(auth, email, password)
+                .then((data) => {
+                    console.log(data.user)
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                })
+        }else {
+            createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user)
+            }).catch((error) => {
+                console.log(error.message);
+            })
+        }
 
     }
 
@@ -39,12 +57,15 @@ function Login() {
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-xl-3 col-md-5 col-sm-7 col-10 login-parent">
-                        <h4 className="text-center">LogIn</h4>
+                        <div className="changer-btns d-flex">
+                            <button className={inUp? "active": ""} onClick={handleChangeFirst}>SignIn</button>
+                            <button className={inUp? "": "active"} onClick={handleChangeSecond}>SignUp</button>
+                        </div>
                         <form onSubmit={(e) => {loginWithEmailPassword(e)}}>
                             <input type="email" placeholder="Enter_your_email_name_@..." onChange={(e) => {getEmVal(e)}}/>
                             <input type="password" placeholder="Enter_your_email_password****" onChange={(e) => {getPassVal(e)}}/>
                             <button className="enter-btn">Enter <i className="bi bi-hand-index-thumb"></i></button>
-                            <button className="enter-btn-2" onClick={(e) => {loginPopup(e)}}>LogIn with <i className="bi bi-google"></i></button>
+                            <button className="enter-btn-2" onClick={(e) => {loginPopup(e)}}>SignIn with <i className="bi bi-google"></i></button>
                         </form>
                     </div>
                 </div>

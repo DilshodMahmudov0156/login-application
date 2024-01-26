8e8e8a4ffa630a148dda43ad03c1af3fbaf7ec5c
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-26 09:29:14 +0500

    i have tried to use context function

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import ProtectedRoute from "../protected-route/protectedRoute";
 import Profile from "../profile/Profile";
 import Login from "../login-page/Login";
+import {Context} from "../../context/context";
 
 function Body({}) {
+    const { state, dispatch } = useContext(Context);
+
     return (
         <div className="my-body-tag">
-
             <Router>
                 <Routes>
                     <Route element={<ProtectedRoute/>}>
--- src/context/context.js
+++ src/context/context.js
@@ -1,8 +1,26 @@
-import { createContext } from "react";
+import {createContext, useReducer} from "react";
+
+const initialValue = {
+    data: [],
+    term: '',
+    filter: ''
+};
 
 export const Context = createContext();
 
+const reducer  = (state = initialValue, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case 'ON_LOGGER':
+            // write some function !
+            break;
+        default:
+            return { state }
+    }
+};
+
 const Provider = ({children}) => {
-    return <Context.Provider>{children}</Context.Provider>;
-}
+    const [state, dispatch] = useReducer(reducer, initialValue);
+    return <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>;
+};
 export default Provider;

3590b19461c5c05932045b89f56fce80c20c49db
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-28 23:04:35 +0500

    i also removed image-url in profile-circle

--- src/components/profile/rightSide.js
+++ src/components/profile/rightSide.js
@@ -11,7 +11,7 @@ function RightSide({}) {
         <div className="right-side">
             <div className="profile-circle">
                 {/*<img src="https://neweralive.na/storage/images/2023/may/lloyd-sikeba.jpg" alt=""/>*/}
-                <img src="https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg" alt=""/>
+                {/*<img src="" alt=""/>*/}
             </div>
             <button className="profile-image-editor">
                 <i className="bi bi-image"></i> <i className="bi bi-arrow-left-right"></i> <i className="bi bi-image"></i>

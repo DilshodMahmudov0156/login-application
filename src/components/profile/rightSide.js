b2f907a009d8a2616038d869e243f79a9d5c80a7
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-29 23:35:00 +0500

    i have deleted variables and functions of uploaad

--- src/components/profile/rightSide.js
+++ src/components/profile/rightSide.js
@@ -1,17 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import { db } from "../../fierbase/firebaseConfig";
 import { v4 as uuidv4} from "uuid";
 import { set, ref } from "firebase/database";
 
 
 function RightSide() {
-
-    const [img, setImg] = useState('');
-
-    const logOut = () => {
-        localStorage.clear();
-        window.location.reload();
-    }
 
 
     const putImage = () => {
@@ -38,7 +31,6 @@ function RightSide() {
             <button className="profile-image-editor" onClick={putImage}>
                 Change <i className="bi bi-image"></i>
             </button>
-            <input type="file" onChange={(e)=>{setImg(e.target.files[0])}}/>
 
 
         </div>

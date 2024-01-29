e11393f4d6d33910f21f16985a2440b0ac870b87
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-29 22:24:22 +0500

    i have just started to work on my own systemic program

--- src/components/profile/rightSide.js
+++ src/components/profile/rightSide.js
@@ -11,7 +11,7 @@ function RightSide({}) {
         <div className="right-side">
             <div className="profile-circle">
                 {/*<img src="https://neweralive.na/storage/images/2023/may/lloyd-sikeba.jpg" alt=""/>*/}
-                {/*<img src="" alt=""/>*/}
+                <img src="" alt=""/>
             </div>
             <button className="profile-image-editor">
                 <i className="bi bi-image"></i> <i className="bi bi-arrow-left-right"></i> <i className="bi bi-image"></i>

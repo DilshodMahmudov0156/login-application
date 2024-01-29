871b40c25afbb47d3ec6871803c2e8e5922d5bd3
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-01-29 23:33:05 +0500

    i have used with firebase database

--- src/components/profile/rightSide.js
+++ src/components/profile/rightSide.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import { db } from "../../fierbase/firebaseConfig";
+import { v4 as uuidv4} from "uuid";
+import { set, ref } from "firebase/database";
 
-function RightSide({}) {
+
+function RightSide() {
+
+    const [img, setImg] = useState('');
 
     const logOut = () => {
         localStorage.clear();
         window.location.reload();
+    }
+
+
+    const putImage = () => {
+
+        set(ref(db, `/${uuidv4()}`), {
+            id: uuidv4(),
+            url: ''
+        })
+            .then((data) => {
+            console.log()
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
     }
 
     return (
@@ -13,9 +35,10 @@ function RightSide({}) {
                 {/*<img src="https://neweralive.na/storage/images/2023/may/lloyd-sikeba.jpg" alt=""/>*/}
                 <img src="" alt=""/>
             </div>
-            <button className="profile-image-editor">
+            <button className="profile-image-editor" onClick={putImage}>
                 Change <i className="bi bi-image"></i>
             </button>
+            <input type="file" onChange={(e)=>{setImg(e.target.files[0])}}/>
 
 
         </div>
--- src/fierbase/firebaseConfig.js
+++ src/fierbase/firebaseConfig.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCqAQrzwejQ7jXri5khJkrgkcpBFz8_h-Y",
     authDomain: "perfect-login-f86b1.firebaseapp.com",
+    databaseURL: "https://perfect-login-f86b1-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "perfect-login-f86b1",
     storageBucket: "perfect-login-f86b1.appspot.com",
     messagingSenderId: "891477804222",
@@ -14,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const db = getDatabase(app);
 
-export {auth, provider};
+export { auth, provider, db};
